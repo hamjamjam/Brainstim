b0fd4690a326355e01966096f98a77e8bd5609d1
abdu5612 <64446217+abdu5612@users.noreply.github.com>
2020-05-29 12:20:50 -0600

    5/29/20
    Cleaning up the folder

--- AnalysisScripts/ClassificationJudging/logisticRegression_Abby/thresholds_above_sham.m
+++
@@ -1,6 +0,0 @@
-function [features] = thresholds_above_sham(i,thresholds,features,sham)
-
-        threshabovesham = sum(thresholds(2:end,:) > sham);
-        features(i,2) = threshabovesham;
-        
-end
